cc91fdb668d8ea3fa76873870ef21c75534556cd
Mehsan-Ali <mehsanaliaziz@gmail.com>
2025-08-08 00:48:36 +0500

    Working on the product page and aj k liye bohot hy

--- frontend/src/pages/Product.tsx
+++ frontend/src/pages/Product.tsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import { useParams } from 'react-router-dom'
+import HeroSection from '../components/product/HeroSection'
+// import { HeroSection } from '../components/product/HeroSection'
 
 export const Product = () => {
+  const { productId } = useParams()
+
   return (
-    <div>Product</div>
+    <div>
+      <HeroSection/>
+
+    </div>
   )
 }
