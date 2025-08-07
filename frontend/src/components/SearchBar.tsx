620c4ae292f8515cf8c0465103f4ae09fe75efdb
Mehsan-Ali <mehsanaliaziz@gmail.com>
2025-08-07 22:57:09 +0500

    search functionality optimized

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -11,6 +11,7 @@ export const SearchBar = () => {
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const value = e.target.value
         dispatch(setSearch(value))
+        e.preventDefault()
     }
 
     // const handleSubmit = (e: React.FormEvent) => {
@@ -23,13 +24,13 @@ export const SearchBar = () => {
     return (
         showSearch ?
             <div className='mx-0 sm:mx-28 py-4 flex items-center justify-center bg-gray-100 border-b border-gray-300' >
-                <form className='w-full sm:flex-10/12 flex items-center justify-center'>
+                <div className='w-full sm:flex-10/12 flex items-center justify-center'>
                     <input onChange={handleChange} type="text" name="search" id="search" className='w-10/12 sm:w-1/2 p-2.5 sm:p-3 border border-gray-400 rounded-2xl outline-none placeholder:text-sm' placeholder='Search' />
                     <img src={assets.search_icon} alt="" className='w-4 sm:w-5 -mx-10 cursor-pointer' />
                     <div className='flex items-center'>
                         <img onClick={() => dispatch(setShowSearch(!showSearch))} src={assets.cross_icon} alt="" className='w-3 sm:w-4 ml-20 cursor-pointer' />
                     </div>
-                </form>
+                </div>
             </div> : null
     )
 }
