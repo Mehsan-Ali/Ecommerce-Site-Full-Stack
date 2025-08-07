82c17882a32c4fb2bda339e2bbaec6ba4eb1d1a5
Mehsan-Ali <mehsanaliaziz@gmail.com>
2025-08-07 22:34:41 +0500

    Search Icon visible now

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -7,7 +7,7 @@ export const SearchBar = () => {
     const dispatch = useAppDispatch()
     // const products = useAppSelector((state) => state.shop.products)
     const { showSearch } = useAppSelector((state) => state.shop)
-    
+
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const value = e.target.value
         dispatch(setSearch(value))
@@ -25,9 +25,7 @@ export const SearchBar = () => {
             <div className='mx-0 sm:mx-28 py-4 flex items-center justify-center bg-gray-100 border-b border-gray-300' >
                 <form className='w-full sm:flex-10/12 flex items-center justify-center'>
                     <input onChange={handleChange} type="text" name="search" id="search" className='w-10/12 sm:w-1/2 p-2.5 sm:p-3 border border-gray-400 rounded-2xl outline-none placeholder:text-sm' placeholder='Search' />
-                    <button type='submit'>
-                        <img src={assets.search_icon} alt="" className='w-4 sm:w-5 -mx-10 cursor-pointer' />
-                    </button>
+                    <img src={assets.search_icon} alt="" className='w-4 sm:w-5 -mx-10 cursor-pointer' />
                     <div className='flex items-center'>
                         <img onClick={() => dispatch(setShowSearch(!showSearch))} src={assets.cross_icon} alt="" className='w-3 sm:w-4 ml-20 cursor-pointer' />
                     </div>
