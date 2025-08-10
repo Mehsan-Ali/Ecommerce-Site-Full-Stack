875864ed682c9dd698b25548bd955e0b2dd0d000
Mehsan-Ali <mehsanaliaziz@gmail.com>
2025-08-10 17:30:28 +0500

    Auth Pages Design completed

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,25 +1,32 @@
-import React from 'react'
 import { Title } from '../components/Title'
 import { NavLink } from 'react-router-dom'
 
 export const Login = () => {
+    const omSubmitHandler = async (e: React.FormEvent) => { 
+        e.preventDefault()
+        try {
+            alert('Login Successfull')
+        } catch (error) {
+            console.error(error);
+        }
+    }
     return (
         <div className='flex flex-col justify-center items-center min-h-screen'>
-            <div className='text-2xl w-full max-w-lg text-center'>
+            <div className='font-light text-4xl w-full max-w-lg text-center py-10'>
                 <Title text1='Login' text2='' />
             </div>
-            <form className='flex flex-col gap-5 w-full max-w-lg'>
-                <input type="text" name="email" id="email" placeholder='Email' className='border border-gray-400 rounded-md py-2 px-2 focus:outline-gray-200' />
-                <input type="text" name="password" id="password" placeholder='Password' className='border border-gray-400 rounded-md py-2 px-2 focus:outline-gray-200' />
+            <form onSubmit={omSubmitHandler} className='flex flex-col gap-5 w-full max-w-lg'>
+                <input type="text" name="email" id="email" placeholder='Email' className='border border-gray-400 rounded-md py-2 px-2 sm:px-4 focus:outline-gray-200' />
+                <input type="password" name="password" id="password" placeholder='Password' className='border border-gray-400 rounded-md py-2 px-2 sm:px-4 focus:outline-gray-200' />
                 <button type='submit' className='bg-black cursor-pointer text-white py-2 rounded-md'>
-                    Sign in
+                    Sign In
                 </button>
                 <div className='text-sm flex justify-between'>
                     <NavLink to={'forget-password'}>
                         Forgot your password?
                     </NavLink>
-                    <NavLink to={'forget-password'}>
-                        Forgot your password?
+                    <NavLink to={'/signup'}>
+                        Create Account
                     </NavLink>
                 </div>
             </form>
--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,7 +1,39 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { Title } from '../components/Title'
 
 export const Signup = () => {
-  return (
-    <div>Signup</div>
-  )
+    const omSubmitHandler = async (e: React.FormEvent) => { 
+        e.preventDefault()
+        try {
+            alert('Login Successfull')
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    return (
+        <div>
+            <div className='flex flex-col justify-center items-center min-h-screen'>
+                <div className='font-light text-4xl w-full max-w-lg text-center py-10'>
+                    <Title text1='Signup' text2='' />
+                </div>
+                <form onSubmit={omSubmitHandler} className='flex flex-col gap-5 w-full max-w-lg'>
+                    <input type="text" name="name" id="name" placeholder='Name' className='border border-gray-400 rounded-md py-2 px-2 sm:px-4 focus:outline-gray-200' />
+                    <input type="text" name="email" id="email" placeholder='Email' className='border border-gray-400 rounded-md py-2 px-2 sm:px-4 focus:outline-gray-200' />
+                    <input type="password" name="password" id="password" placeholder='Password' className='border border-gray-400 rounded-md py-2 px-2 sm:px-4 focus:outline-gray-200' />
+                    <button type='submit' className='bg-black cursor-pointer text-white py-2 rounded-md'>
+                        Sign Up
+                    </button>
+                    <div className='text-sm flex justify-between'>
+                        <NavLink to={'forget-password'}>
+                            Forgot your password?
+                        </NavLink>
+                        <NavLink to={'/login'}>
+                            Already have an account
+                        </NavLink>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
 }
