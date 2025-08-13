394dc73e195751ace0e03186683418c193f739f7
Mehsan-Ali <mehsanaliaziz@gmail.com>
2025-08-13 21:26:56 +0500

    Loading State aslo managed In the Home Page

--- frontend/src/components/BestSeller.tsx
+++ frontend/src/components/BestSeller.tsx
@@ -7,10 +7,15 @@ import { ProductItem } from './ProductItem'
 export const BestSeller = () => {
     const products = useAppSelector((state) => state.shop.products)
     const [bestProducts, setBestProducts] = useState<Product[]>([])
-    console.log(products)
+    const [isLoading, setIsLoading] = useState<boolean>(true)
+
     useEffect(() => {
-        const bestProduct = products.filter((item) => item.bestSeller).slice(0,5)
-        setBestProducts(bestProduct)
+        setIsLoading(true)
+        setTimeout(() => {
+            const bestProduct = products.filter((item) => item.bestSeller).slice(0, 5)
+            setBestProducts(bestProduct)
+            setIsLoading(false)
+        }, 2000);
     }, [products])
 
     console.log(bestProducts)
@@ -21,15 +26,22 @@ export const BestSeller = () => {
                 <Title text1='Best' text2='Seller' />
                 <p className='w-3/4 sm:w-1/2 mx-auto text-center text-gray-500 text-sm'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Qui culpa repellendus, suscipit a numquam cum dolor doloribus optio officiis.</p>
             </div>
+            {
+                isLoading ?
+                    <div className='flex justify-center text-gray-400 items-center gap-2'>
+                        <div className='border-2 size-8 border-gray-400 border-t-transparent rounded-full animate-spin'></div>
+                        <p>Loading Products...</p>
+                    </div> :
+                    // {/* --------- Rendering Best Seller Products --------- */}
+                    <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6 px-5 sm:px-10'>
+                        {
+                            bestProducts.map((product, index) => (
+                                <ProductItem key={index} {...product} />
+                            ))
+                        }
+                    </div>
+            }
 
-            {/* --------- Rendering Best Seller Products --------- */}
-            <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6 px-5 sm:px-10'>
-                {
-                    bestProducts.map((product, index) => (
-                        <ProductItem key={index} {...product} />
-                    ))
-                }
-            </div>
         </div>
     )
 }
--- frontend/src/components/LatestCollections.tsx
+++ frontend/src/components/LatestCollections.tsx
@@ -7,8 +7,14 @@ import { ProductItem } from './ProductItem'
 export const LatestCollections = () => {
   const products = useAppSelector(state => state.shop.products)
   const [latestProducts, setLatestProducts] = useState<Product[]>([])
+  const [isLoading, setIsLoading] = useState<boolean>(true)
 
   useEffect(() => {
+    setIsLoading(true)
+    setTimeout(() => {
+      setLatestProducts(products.slice(0, 10))
+      setIsLoading(false)
+    }, 3000);
     setLatestProducts(products.slice(0, 10))
   }, [products])
 
@@ -18,15 +24,23 @@ export const LatestCollections = () => {
         <Title text1='Latest' text2='Collections' />
         <p className='text-gray-500 text-sm w-3/4 sm:w-1/2 mx-auto'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti nisi praesentium ut numquam quis</p>
       </div>
+      {
 
-      {/* --------- Rendering Products --------- */}
-      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6 px-5 sm:px-10'>
-        {
-          latestProducts.map((product,index) => (
-              <ProductItem key={index} {...product} />
-          ))
-        }
-      </div>
+        isLoading ?
+          <div className='flex justify-center gap-2 w-full items-center h-64'>
+            <div className='border-2 border-t-transparent border-gray-400 size-8 rounded-full animate-spin'>
+            </div>
+            <p className='text-gray-500 text-lg'>Loading latest products...</p>
+          </div> :
+          // {/* --------- Rendering Products --------- */}
+          <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6 px-5 sm:px-10'>
+            {
+              latestProducts.map((product, index) => (
+                <ProductItem key={index} {...product} />
+              ))
+            }
+          </div>
+      }
     </div>
   )
 }
